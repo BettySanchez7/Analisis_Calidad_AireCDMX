9101f627b64f75cdf52f354a87c2d51f66b5c637
tardigrado-visual <danielramoslopez@hotmail.com>
2021-02-03 12:02:46 -0600

    Actualización orden año,mes

--- datasets/lecturaDatos.R
+++ datasets/lecturaDatos.R
@@ -1,6 +1,9 @@
+#LECTURA Y LIMPIEZA DE DATOS
+
 library(dplyr)
 library(ggplot2)
 library(e1071)
+library(lubridate) #Paquete para manipular fechas
 
 #Seleccionando carpeta de trabajo
 setwd("Documents/Repositorios/Analisis_Calidad_AireCDMX")
@@ -12,9 +15,9 @@ LecturaCSV <- function(url,sep){
 }
 
 ##Unión de datasets  
-##Se unen los datasets y se renombra columnas para que coincidad
-##Se seleccionan las columnas necesarias para trabajar 
-##UIZ, PED no están en todos los datasets (P10 Y PM25)
+##Se unen los datasets y se renombra columnas para que coincidan
+##Se seleccionan las estaciones que están activas durante el periodo de tiempo que 
+##considera el estudio y con sufientes datos para trabajar
 uniondf <- function(data1,data2=NULL){
   DATA1 <- lapply(c(data1,data2), select, c(contains("FECHA"), SAG, TLA, XAL, MER, PED, UIZ))
   n = length(DATA1)
@@ -31,10 +34,10 @@ orden <- function(datos){
   datos[order(as.Date(datos$FECHA, format="%d/%m/%Y")),]
 }
 
-##Agrupando por años, se obtiene el promedio de las columnas por estación, se agrupa por mes###
+##Agrupando por mes, se obtiene el promedio mensual para cada estación ###
 groupmeses <- function(dataframe){
   data_mes <- mutate(dataframe, FECHA= as.Date(FECHA, "%d/%m/%Y"))
-  data_mes <- mutate(data_mes, mes= format(FECHA,"%m/%Y"))
+  data_mes <- mutate(data_mes, mes= floor_date(data_mes$FECHA, "month"))
   result <- data_mes %>%  group_by(mes) %>%
     summarise(mean.TLA=mean(TLA, na.rm = TRUE), mean.MER=mean(MER, na.rm = TRUE),
               mean.UIZ=mean(UIZ, na.rm = TRUE),mean.PED=mean(PED, na.rm = TRUE),
@@ -61,7 +64,7 @@ O3 <- uniondf(O3)
 SO2 <- LecturaCSV("datasets/datasets_contaminantes/SO2/",",")
 SO2 <- uniondf(SO2)
 
-##################################ORDENANDO DATOS POR EL AÑO########################
+##################################ORDENANDO DATOS POR EL AÑO,MES########################
 ##ORDENANDO DATOS
 CO <- orden(CO)
 SO2 <- orden(SO2)
@@ -86,11 +89,7 @@ CO <- mutate(CO, TLA= as.numeric(TLA), MER= as.numeric(MER),
             )
 CO[CO =="-99"] <- NA
 
-#Leyendo número de datos faltantes
-sum(is.na(NO2))
-sum(is.na(NO2))
-
-##Agrupando por años
+##Agrupando por meses
 
 NO2 <- groupmeses(NO2)
 O3 <- groupmeses(O3)
@@ -99,11 +98,9 @@ SO2 <- groupmeses(SO2)
 
 ##Creando csv
 
-write.csv(NO2, file="Documents/Repositorios/Analisis_Calidad_AireCDMX/datasets/NO2.csv", row.names = FALSE)
-write.csv(O3, file="Documents/Repositorios/Analisis_Calidad_AireCDMX/datasets/O3.csv", row.names = FALSE)
-write.csv(CO, file="Documents/Repositorios/Analisis_Calidad_AireCDMX/datasets/CO.csv", row.names = FALSE)
-write.csv(SO2, file="Documents/Repositorios/Analisis_Calidad_AireCDMX/datasets/SO2.csv", row.names = FALSE)
-
-
+write.csv(NO2, file="datasets/contaminantes_mensual/NO2.csv", row.names = FALSE)
+write.csv(O3, file="datasets/contaminantes_mensual/O3.csv", row.names = FALSE)
+write.csv(CO, file="datasets/contaminantes_mensual/CO.csv", row.names = FALSE)
+write.csv(SO2, file="datasets/contaminantes_mensual/SO2.csv", row.names = FALSE)
 
 ##############################################################################################3
