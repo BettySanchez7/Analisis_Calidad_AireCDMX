e8f960085f175db185315bbe1f94bad9cea79f72
tardigrado-visual <danielramoslopez@hotmail.com>
2021-02-05 21:41:11 -0600

    Todas las series con su tendencia

--- series_tiempo.R
+++ series_tiempo.R
@@ -148,80 +148,387 @@ ts.plot(SO.PM10.ts)
 ts.plot(SE.PM10.ts)
 dev.off()
 
+#################################### GRÁFICAS CON TENDENCIA ####################################
 
-#### Descomposición de series para la Zona Centro
+################## Centro (CE)
 #CO
 CE.CO.comp <-decompose(CE.CO.ts,type = "mult")
 #plot(CE.CO.comp)
-
 trendCO <- as.numeric(CE.CO.comp$trend)
 
 ggplot(CO,aes(x=mes)) +
   geom_line(aes(y=CE,colour = "Serie de tiempo"))+
   geom_line(aes(y=trendCO,colour = "Tendencia"), lwd = 0.9)+
   scale_colour_manual("", breaks = c("Serie de tiempo", "Tendencia"),
-                      values = c("black", "darkred")) +
-  labs(title = "Concentración de CO - Zona Centro", y= "Valor IMECA   [0-200]",
+                      values = c("black","#d35400")) +
+  labs(title = "Concentración de CO - Zona Centro", y= "Concentración [unidades IMECA]",
        x = "Tiempo",subtitle = "Serie de tiempo de Enero 2005 - Diciembre 2020")+
+  ylim(0,30)+
   theme_classic()
 
 #NO2
 CE.NO2.comp <-decompose(CE.NO2.ts,type = "mult")
 #plot(CE.CO.comp)
-
 trendNO2 <- as.numeric(CE.NO2.comp$trend)
 
 ggplot(NO2,aes(x=mes)) +
   geom_line(aes(y=CE,colour = "Serie de tiempo"))+
   geom_line(aes(y=trendNO2,colour = "Tendencia"), lwd = 0.9)+
   scale_colour_manual("", breaks = c("Serie de tiempo", "Tendencia"),
-                      values = c("black", "darkred")) +
-  labs(title = "Concentración de NO2 - Zona Centro", y= "Valor IMECA   [0-200]",
+                      values = c("black", "#2471a3")) +
+  labs(title = "Concentración de NO2 - Zona Centro", y= "Concentración [unidades IMECA]",
        x = "Tiempo",subtitle = "Serie de tiempo de Enero 2005 - Diciembre 2020")+
+  ylim(5,35)+
   theme_classic()
 
 #SO2
 CE.SO2.comp <-decompose(CE.SO2.ts,type = "mult")
 #plot(CE.CO.comp)
-
 trendSO2 <- as.numeric(CE.SO2.comp$trend)
 
 ggplot(SO2,aes(x=mes)) +
   geom_line(aes(y=CE,colour = "Serie de tiempo"))+
   geom_line(aes(y=trendSO2,colour = "Tendencia"), lwd = 0.9)+
   scale_colour_manual("", breaks = c("Serie de tiempo", "Tendencia"),
-                      values = c("black", "darkred")) +
-  labs(title = "Concentración de SO2 - Zona Centro", y= "Valor IMECA   [0-200]",
+                      values = c("black", "#1e8449")) +
+  labs(title = "Concentración de SO2 - Zona Centro", y= "Concentración [unidades IMECA]",
        x = "Tiempo",subtitle = "Serie de tiempo de Enero 2005 - Diciembre 2020")+
+  ylim(0,30)+
   theme_classic()
 
 #O3
 CE.O3.comp <-decompose(CE.O3.ts,type = "mult")
 #plot(CE.CO.comp)
-
 trendO3 <- as.numeric(CE.O3.comp$trend)
 
 ggplot(O3,aes(x=mes)) +
   geom_line(aes(y=CE,colour = "Serie de tiempo"))+
   geom_line(aes(y=trendO3,colour = "Tendencia"), lwd = 0.9)+
   scale_colour_manual("", breaks = c("Serie de tiempo", "Tendencia"),
-                      values = c("black", "darkred")) +
-  labs(title = "Concentración de O3 - Zona Centro", y= "Valor IMECA   [0-200]",
+                      values = c("black", "#e74c3c")) +
+  labs(title = "Concentración de O3 - Zona Centro", y= "Concentración [unidades IMECA]",
        x = "Tiempo",subtitle = "Serie de tiempo de Enero 2005 - Diciembre 2020")+
+  ylim(15,60)+
   theme_classic()
 
 #PM10
 CE.PM10.comp <-decompose(CE.PM10.ts,type = "mult")
 #plot(CE.CO.comp)
-
 trendPM10 <- as.numeric(CE.PM10.comp$trend)
 
 ggplot(PM10,aes(x=mes)) +
   geom_line(aes(y=CE,colour = "Serie de tiempo"))+
   geom_line(aes(y=trendPM10,colour = "Tendencia"), lwd = 0.9)+
   scale_colour_manual("", breaks = c("Serie de tiempo", "Tendencia"),
-                      values = c("black", "darkred")) +
-  labs(title = "Concentración de PM10 - Zona Centro", y= "Valor IMECA   [0-200]",
+                      values = c("black", "#7b241c")) +
+  labs(title = "Concentración de PM10 - Zona Centro", y= "Concentración [unidades IMECA]",
        x = "Tiempo",subtitle = "Serie de tiempo de Enero 2005 - Diciembre 2020")+
+  ylim(15,105)+
   theme_classic()
 
+################## Noroeste (NO)
+#CO
+NO.CO.comp <-decompose(NO.CO.ts,type = "mult")
+#plot(NO.CO.comp)
+trendCO <- as.numeric(NO.CO.comp$trend)
+
+ggplot(CO,aes(x=mes)) +
+  geom_line(aes(y=NO,colour = "Serie de tiempo"))+
+  geom_line(aes(y=trendCO,colour = "Tendencia"), lwd = 0.9)+
+  scale_colour_manual("", breaks = c("Serie de tiempo", "Tendencia"),
+                      values = c("black","#d35400")) +
+  labs(title = "Concentración de CO - Zona Noroeste", y= "Concentración [unidades IMECA]",
+       x = "Tiempo",subtitle = "Serie de tiempo de Enero 2005 - Diciembre 2020")+
+  ylim(0,30)+
+  theme_classic()
+
+
+#NO2
+NO.NO2.comp <-decompose(NO.NO2.ts,type = "mult")
+#plot(NO.CO.comp)
+trendNO2 <- as.numeric(NO.NO2.comp$trend)
+
+ggplot(NO2,aes(x=mes)) +
+  geom_line(aes(y=NO,colour = "Serie de tiempo"))+
+  geom_line(aes(y=trendNO2,colour = "Tendencia"), lwd = 0.9)+
+  scale_colour_manual("", breaks = c("Serie de tiempo", "Tendencia"),
+                      values = c("black", "#2471a3")) +
+  labs(title = "Concentración de NO2 - Zona Noroeste", y= "Concentración [unidades IMECA]",
+       x = "Tiempo",subtitle = "Serie de tiempo de Enero 2005 - Diciembre 2020")+
+  ylim(5,35)+
+  theme_classic()
+
+#SO2
+NO.SO2.comp <-decompose(NO.SO2.ts,type = "mult")
+#plot(NO.CO.comp)
+trendSO2 <- as.numeric(NO.SO2.comp$trend)
+
+ggplot(SO2,aes(x=mes)) +
+  geom_line(aes(y=NO,colour = "Serie de tiempo"))+
+  geom_line(aes(y=trendSO2,colour = "Tendencia"), lwd = 0.9)+
+  scale_colour_manual("", breaks = c("Serie de tiempo", "Tendencia"),
+                      values = c("black", "#1e8449")) +
+  labs(title = "Concentración de SO2 - Zona Noroeste", y= "Concentración [unidades IMECA]",
+       x = "Tiempo",subtitle = "Serie de tiempo de Enero 2005 - Diciembre 2020")+
+  ylim(0,30)+
+  theme_classic()
+
+#O3
+NO.O3.comp <-decompose(NO.O3.ts,type = "mult")
+#plot(NO.CO.comp)
+trendO3 <- as.numeric(NO.O3.comp$trend)
+
+ggplot(O3,aes(x=mes)) +
+  geom_line(aes(y=NO,colour = "Serie de tiempo"))+
+  geom_line(aes(y=trendO3,colour = "Tendencia"), lwd = 0.9)+
+  scale_colour_manual("", breaks = c("Serie de tiempo", "Tendencia"),
+                      values = c("black", "#e74c3c")) +
+  labs(title = "Concentración de O3 - Zona Noroeste", y= "Concentración [unidades IMECA]",
+       x = "Tiempo",subtitle = "Serie de tiempo de Enero 2005 - Diciembre 2020")+
+  ylim(15,60)+
+  theme_classic()
+
+#PM10
+NO.PM10.comp <-decompose(NO.PM10.ts,type = "mult")
+#plot(NO.CO.comp)
+trendPM10 <- as.numeric(NO.PM10.comp$trend)
+
+ggplot(PM10,aes(x=mes)) +
+  geom_line(aes(y=NO,colour = "Serie de tiempo"))+
+  geom_line(aes(y=trendPM10,colour = "Tendencia"), lwd = 0.9)+
+  scale_colour_manual("", breaks = c("Serie de tiempo", "Tendencia"),
+                      values = c("black", "#7b241c")) +
+  labs(title = "Concentración de PM10 - Zona Noroeste", y= "Concentración [unidades IMECA]",
+       x = "Tiempo",subtitle = "Serie de tiempo de Enero 2005 - Diciembre 2020")+
+  ylim(15,105)+
+  theme_classic()
+
+################## Noreste (NE)
+#CO
+NE.CO.comp <-decompose(NE.CO.ts,type = "mult")
+#plot(NE.CO.comp)
+trendCO <- as.numeric(NE.CO.comp$trend)
+
+ggplot(CO,aes(x=mes)) +
+  geom_line(aes(y=NE,colour = "Serie de tiempo"))+
+  geom_line(aes(y=trendCO,colour = "Tendencia"), lwd = 0.9)+
+  scale_colour_manual("", breaks = c("Serie de tiempo", "Tendencia"),
+                      values = c("black","#d35400")) +
+  labs(title = "Concentración de CO - Zona Noreste", y= "Concentración [unidades IMECA]",
+       x = "Tiempo",subtitle = "Serie de tiempo de Enero 2005 - Diciembre 2020")+
+  ylim(0,30)+
+  theme_classic()
+
+
+#NO2
+NE.NO2.comp <-decompose(NE.NO2.ts,type = "mult")
+#plot(NE.CO.comp)
+trendNO2 <- as.numeric(NE.NO2.comp$trend)
+
+ggplot(NO2,aes(x=mes)) +
+  geom_line(aes(y=NE,colour = "Serie de tiempo"))+
+  geom_line(aes(y=trendNO2,colour = "Tendencia"), lwd = 0.9)+
+  scale_colour_manual("", breaks = c("Serie de tiempo", "Tendencia"),
+                      values = c("black", "#2471a3")) +
+  labs(title = "Concentración de NO2 - Zona Noreste", y= "Concentración [unidades IMECA]",
+       x = "Tiempo",subtitle = "Serie de tiempo de Enero 2005 - Diciembre 2020")+
+  ylim(5,35)+
+  theme_classic()
+
+#SO2
+NE.SO2.comp <-decompose(NE.SO2.ts,type = "mult")
+#plot(NE.CO.comp)
+trendSO2 <- as.numeric(NE.SO2.comp$trend)
+
+ggplot(SO2,aes(x=mes)) +
+  geom_line(aes(y=NE,colour = "Serie de tiempo"))+
+  geom_line(aes(y=trendSO2,colour = "Tendencia"), lwd = 0.9)+
+  scale_colour_manual("", breaks = c("Serie de tiempo", "Tendencia"),
+                      values = c("black", "#1e8449")) +
+  labs(title = "Concentración de SO2 - Zona Noreste", y= "Concentración [unidades IMECA]",
+       x = "Tiempo",subtitle = "Serie de tiempo de Enero 2005 - Diciembre 2020")+
+  ylim(0,30)+
+  theme_classic()
+
+#O3
+NE.O3.comp <-decompose(NE.O3.ts,type = "mult")
+#plot(NE.CO.comp)
+trendO3 <- as.numeric(NE.O3.comp$trend)
+
+ggplot(O3,aes(x=mes)) +
+  geom_line(aes(y=NE,colour = "Serie de tiempo"))+
+  geom_line(aes(y=trendO3,colour = "Tendencia"), lwd = 0.9)+
+  scale_colour_manual("", breaks = c("Serie de tiempo", "Tendencia"),
+                      values = c("black", "#e74c3c")) +
+  labs(title = "Concentración de O3 - Zona Noreste", y= "Concentración [unidades IMECA]",
+       x = "Tiempo",subtitle = "Serie de tiempo de Enero 2005 - Diciembre 2020")+
+  ylim(15,60)+
+  theme_classic()
+
+#PM10
+NE.PM10.comp <-decompose(NE.PM10.ts,type = "mult")
+#plot(NE.CO.comp)
+trendPM10 <- as.numeric(NE.PM10.comp$trend)
+
+ggplot(PM10,aes(x=mes)) +
+  geom_line(aes(y=NE,colour = "Serie de tiempo"))+
+  geom_line(aes(y=trendPM10,colour = "Tendencia"), lwd = 0.9)+
+  scale_colour_manual("", breaks = c("Serie de tiempo", "Tendencia"),
+                      values = c("black", "#7b241c")) +
+  labs(title = "Concentración de PM10 - Zona Noreste", y= "Concentración [unidades IMECA]",
+       x = "Tiempo",subtitle = "Serie de tiempo de Enero 2005 - Diciembre 2020")+
+  ylim(15,120)+
+  theme_classic()
+
+################## Suroeste (SO)
+#CO
+SO.CO.comp <-decompose(SO.CO.ts,type = "mult")
+#plot(SO.CO.comp)
+trendCO <- as.numeric(SO.CO.comp$trend)
+
+ggplot(CO,aes(x=mes)) +
+  geom_line(aes(y=SO,colour = "Serie de tiempo"))+
+  geom_line(aes(y=trendCO,colour = "Tendencia"), lwd = 0.9)+
+  scale_colour_manual("", breaks = c("Serie de tiempo", "Tendencia"),
+                      values = c("black","#d35400")) +
+  labs(title = "Concentración de CO - Zona Suroeste", y= "Concentración [unidades IMECA]",
+       x = "Tiempo",subtitle = "Serie de tiempo de Enero 2005 - Diciembre 2020")+
+  ylim(0,30)+
+  theme_classic()
+
+
+#NO2
+SO.NO2.comp <-decompose(SO.NO2.ts,type = "mult")
+#plot(SO.CO.comp)
+trendNO2 <- as.numeric(SO.NO2.comp$trend)
+
+ggplot(NO2,aes(x=mes)) +
+  geom_line(aes(y=SO,colour = "Serie de tiempo"))+
+  geom_line(aes(y=trendNO2,colour = "Tendencia"), lwd = 0.9)+
+  scale_colour_manual("", breaks = c("Serie de tiempo", "Tendencia"),
+                      values = c("black", "#2471a3")) +
+  labs(title = "Concentración de NO2 - Zona Suroeste", y= "Concentración [unidades IMECA]",
+       x = "Tiempo",subtitle = "Serie de tiempo de Enero 2005 - Diciembre 2020")+
+  ylim(5,35)+
+  theme_classic()
+
+#SO2
+SO.SO2.comp <-decompose(SO.SO2.ts,type = "mult")
+#plot(SO.CO.comp)
+trendSO2 <- as.numeric(SO.SO2.comp$trend)
+
+ggplot(SO2,aes(x=mes)) +
+  geom_line(aes(y=SO,colour = "Serie de tiempo"))+
+  geom_line(aes(y=trendSO2,colour = "Tendencia"), lwd = 0.9)+
+  scale_colour_manual("", breaks = c("Serie de tiempo", "Tendencia"),
+                      values = c("black", "#1e8449")) +
+  labs(title = "Concentración de SO2 - Zona Suroeste", y= "Concentración [unidades IMECA]",
+       x = "Tiempo",subtitle = "Serie de tiempo de Enero 2005 - Diciembre 2020")+
+  ylim(0,30)+
+  theme_classic()
+
+#O3
+SO.O3.comp <-decompose(SO.O3.ts,type = "mult")
+#plot(SO.CO.comp)
+trendO3 <- as.numeric(SO.O3.comp$trend)
+
+ggplot(O3,aes(x=mes)) +
+  geom_line(aes(y=SO,colour = "Serie de tiempo"))+
+  geom_line(aes(y=trendO3,colour = "Tendencia"), lwd = 0.9)+
+  scale_colour_manual("", breaks = c("Serie de tiempo", "Tendencia"),
+                      values = c("black", "#e74c3c")) +
+  labs(title = "Concentración de O3 - Zona Suroeste", y= "Concentración [unidades IMECA]",
+       x = "Tiempo",subtitle = "Serie de tiempo de Enero 2005 - Diciembre 2020")+
+  ylim(15,60)+
+  theme_classic()
+
+#PM10
+SO.PM10.comp <-decompose(SO.PM10.ts,type = "mult")
+#plot(SO.CO.comp)
+trendPM10 <- as.numeric(SO.PM10.comp$trend)
+
+ggplot(PM10,aes(x=mes)) +
+  geom_line(aes(y=SO,colour = "Serie de tiempo"))+
+  geom_line(aes(y=trendPM10,colour = "Tendencia"), lwd = 0.9)+
+  scale_colour_manual("", breaks = c("Serie de tiempo", "Tendencia"),
+                      values = c("black", "#7b241c")) +
+  labs(title = "Concentración de PM10 - Zona Suroeste", y= "Concentración [unidades IMECA]",
+       x = "Tiempo",subtitle = "Serie de tiempo de Enero 2005 - Diciembre 2020")+
+  ylim(15,105)+
+  theme_classic()
+
+################## Sureste (SE)
+#CO
+SE.CO.comp <-decompose(SE.CO.ts,type = "mult")
+#plot(SE.CO.comp)
+trendCO <- as.numeric(SE.CO.comp$trend)
+
+ggplot(CO,aes(x=mes)) +
+  geom_line(aes(y=SE,colour = "Serie de tiempo"))+
+  geom_line(aes(y=trendCO,colour = "Tendencia"), lwd = 0.9)+
+  scale_colour_manual("", breaks = c("Serie de tiempo", "Tendencia"),
+                      values = c("black","#d35400")) +
+  labs(title = "Concentración de CO - Zona Sureste", y= "Concentración [unidades IMECA]",
+       x = "Tiempo",subtitle = "Serie de tiempo de Enero 2005 - Diciembre 2020")+
+  ylim(0,30)+
+  theme_classic()
+
+#NO2
+SE.NO2.comp <-decompose(SE.NO2.ts,type = "mult")
+#plot(SE.CO.comp)
+trendNO2 <- as.numeric(SE.NO2.comp$trend)
+
+ggplot(NO2,aes(x=mes)) +
+  geom_line(aes(y=SE,colour = "Serie de tiempo"))+
+  geom_line(aes(y=trendNO2,colour = "Tendencia"), lwd = 0.9)+
+  scale_colour_manual("", breaks = c("Serie de tiempo", "Tendencia"),
+                      values = c("black", "#2471a3")) +
+  labs(title = "Concentración de NO2 - Zona Sureste", y= "Concentración [unidades IMECA]",
+       x = "Tiempo",subtitle = "Serie de tiempo de Enero 2005 - Diciembre 2020")+
+  ylim(5,35)+
+  theme_classic()
+
+#SO2
+SE.SO2.comp <-decompose(SE.SO2.ts,type = "mult")
+#plot(SE.CO.comp)
+trendSO2 <- as.numeric(SE.SO2.comp$trend)
+
+ggplot(SO2,aes(x=mes)) +
+  geom_line(aes(y=SE,colour = "Serie de tiempo"))+
+  geom_line(aes(y=trendSO2,colour = "Tendencia"), lwd = 0.9)+
+  scale_colour_manual("", breaks = c("Serie de tiempo", "Tendencia"),
+                      values = c("black", "#1e8449")) +
+  labs(title = "Concentración de SO2 - Zona Sureste", y= "Concentración [unidades IMECA]",
+       x = "Tiempo",subtitle = "Serie de tiempo de Enero 2005 - Diciembre 2020")+
+  ylim(0,30)+
+  theme_classic()
+
+#O3
+SE.O3.comp <-decompose(SE.O3.ts,type = "mult")
+#plot(SE.CO.comp)
+trendO3 <- as.numeric(SE.O3.comp$trend)
+
+ggplot(O3,aes(x=mes)) +
+  geom_line(aes(y=SE,colour = "Serie de tiempo"))+
+  geom_line(aes(y=trendO3,colour = "Tendencia"), lwd = 0.9)+
+  scale_colour_manual("", breaks = c("Serie de tiempo", "Tendencia"),
+                      values = c("black", "#e74c3c")) +
+  labs(title = "Concentración de O3 - Zona Sureste", y= "Concentración [unidades IMECA]",
+       x = "Tiempo",subtitle = "Serie de tiempo de Enero 2005 - Diciembre 2020")+
+  ylim(15,60)+
+  theme_classic()
+
+#PM10
+SE.PM10.comp <-decompose(SE.PM10.ts,type = "mult")
+#plot(SE.CO.comp)
+trendPM10 <- as.numeric(SE.PM10.comp$trend)
+
+ggplot(PM10,aes(x=mes)) +
+  geom_line(aes(y=SE,colour = "Serie de tiempo"))+
+  geom_line(aes(y=trendPM10,colour = "Tendencia"), lwd = 0.9)+
+  scale_colour_manual("", breaks = c("Serie de tiempo", "Tendencia"),
+                      values = c("black", "#7b241c")) +
+  labs(title = "Concentración de PM10 - Zona Sureste", y= "Concentración [unidades IMECA]",
+       x = "Tiempo",subtitle = "Serie de tiempo de Enero 2005 - Diciembre 2020")+
+  ylim(15,105)+
+  theme_classic()
